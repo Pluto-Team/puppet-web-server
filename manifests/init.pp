13989aba66ce8909052574acc1604f92228b6241
lordTocho <brian.highnam@accenturefederaldefense.com>
2020-06-22 09:49:47 -0400

    PLUT-215 added in print statements

--- manifests/init.pp
+++ manifests/init.pp
@@ -8,26 +8,31 @@ class puppet_web_server {
 
   $powershell = 'C:/Windows/System32/WindowsPowerShell/v1.0/powershell.exe -NoProfile -NoLogo -NonInteractive'
 
+  notice( '##### CHANGING TIME ZONES #####' )
   exec { 'change time zones':
     command   => "${powershell} C:/modules/puppet_web_server/files/change-time-zone.ps1",
     logoutput => true,
   }
 
+  notice( '##### ADDING SERVER DEPENDENCIES #####' )
   exec { 'add server dependencies':
     command   => "${powershell} C:/modules/puppet_web_server/files/install-web-server-dependecies.ps1",
     logoutput => true,
   }
 
+  notice( '##### INSTALL WEB SERVER FEATURE #####' )
   exec { 'Install Web Server feature':
     command   => "${powershell} C:/modules/puppet_web_server/files/install-web-server.ps1",
     logoutput => true,
   }
 
+  notice( '##### ADD SERVER TO TARGET GROUP #####' )
   exec { 'add Server Target group':
     command   => "${powershell} C:/modules/puppet_web_server/files/add-server-to-target-group.ps1",
     logoutput => true,
   }
 
+  notice( '##### DEPLOY APP TO IIS #####' )
   exec { 'Deploy app to IIS':
     command   => "${powershell} C:/modules/puppet_web_server/files/deploy-app-to-IIS.ps1",
     logoutput => true,
